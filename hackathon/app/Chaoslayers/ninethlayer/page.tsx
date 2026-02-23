1d166e7fdff54820914756762ff80c9bdeab4f34
Adam Beijar <adam.beijar@gmail.com>
2026-02-23 22:47:56 +0200

    what

--- hackathon/app/Chaoslayers/ninethlayer/page.tsx
+++ hackathon/app/Chaoslayers/ninethlayer/page.tsx
@@ -100,8 +100,76 @@ export default function NinethLayer() {
         }
     };
 
+
+    //ADS
+
+    const [ads, setAds] = useState([]);
+    const [adChoises, setAdChoises] = useState([
+        { name: "Ad 1", src: "/PopUpAds/AffirmYes.png" },
+        { name: "Ad 2", src: "/PopUpAds/SigBeGuru.png" },
+        { name: "Ad 3", src: "/PopUpAds/spelkväll5.png" },
+        { name: "Ad 4", src: "/PopUpAds/StartDatingRichMenNOW.mp4" },
+        { name: "Ad 5", src: "/PopUpAds/VirusDetected.png" },
+        { name: "Ad 6", src: "/PopUpAds/DaTebola.png" },
+        { name: "Ad 7", src: "/PopUpAds/candy.png" },
+        { name: "Ad 8", src: "/PopUpAds/FORGET_YOUR_OLD_BORING_LIFE.mp4" },
+        { name: "Ad 9", src: "/PopUpAds/DownloadACar.png" },
+        { name: "Ad 10", src: "/PopUpAds/Want_fast_computer_1.gif" },
+        { name: "Ad 11", src: "/PopUpAds/Best_Anti-Virus_found_HERE.gif" },
+        { name: "Ad 12", src: "/PopUpAds/hehasbecomethelongboy.png" },
+        { name: "Ad 13", src: "/PopUpAds/You_will_not_belive_what_this_goober_is_accused_of.gif" },
+    ]);
+
+    const removeAd = (indexToRemove: number) => {
+    setAds((prev) => prev.filter((_, index) => index !== indexToRemove));
+    };
+
+    useEffect(() => {
+    let timer: NodeJS.Timeout | undefined;
+
+    const spawnAd = () => {
+        const randomDelay = Math.floor(Math.random() * 8000) + 2000;
+
+        timer = setTimeout(() => {
+        const randomAdIndex = Math.floor(Math.random() * adChoises.length);
+        const screenWidth = window.innerWidth;
+        const screenHeight = window.innerHeight;
+
+        const w = Math.floor(Math.random() * 200) + 300;
+
+        let y = Math.floor(Math.random() * screenHeight);
+        if (screenHeight - y < 250) y = y - 250;
+
+        setAds((prev) => [
+            ...prev,
+            {
+            x: Math.floor(Math.random() * (screenWidth - w)),
+            y,
+            w,
+            src: adChoises[randomAdIndex].src,
+            },
+        ]);
+
+        spawnAd();
+        }, randomDelay);
+    };
+
+    spawnAd();
+    return () => clearTimeout(timer);
+    }, []);
+
     return (
-        <div className="bg-black w-screen h-screen flex flex-col items-center justify-center gap-6">
+        <div className="relative bg-black w-screen h-screen flex items-center justify-center flex-col overflow-hidden">
+            {ads.map((ad, index) => (
+                <PopUpAd
+                    key={index}
+                    x={ad.x}
+                    y={ad.y}
+                    w={ad.w}
+                    src={ad.src}
+                    onClose={() => removeAd(index)}
+                />
+            ))}
             <h1 className="text-white text-3xl font-bold italic">Nionde Lagret</h1>
             <div className="text-white">SKRIV IN DITT LÖSENORD IGEN INOM TIMERN ELLER GÅ TILLBAKS TILL BÖRJAN</div>
             {/* Loading Bar Container */}
